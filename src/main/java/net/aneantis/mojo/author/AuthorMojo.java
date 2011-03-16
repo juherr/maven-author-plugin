4979bb987b1ec50418e632fd7b49750662a34d82
jherr <jherr@jherr-desktop>
2011-03-16 08:22:35 +0100

    change package name and groupId fix sources detection

--- src/main/java/net/aneantis/mojo/author/AuthorMojo.java
+++ src/main/java/net/aneantis/mojo/author/AuthorMojo.java
@@ -1,18 +1,18 @@
-package com.google.code.mojo.author;
+package net.aneantis.mojo.author;
 
 import com.thoughtworks.qdox.JavaDocBuilder;
-import com.thoughtworks.qdox.model.ClassLibrary;
 import com.thoughtworks.qdox.model.DocletTag;
 import com.thoughtworks.qdox.model.JavaClass;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import org.apache.maven.model.Developer;
 import org.apache.maven.project.MavenProject;
 
@@ -20,8 +20,8 @@ import org.apache.maven.project.MavenProject;
  * 
  * @author juherr
  *
- * @goal author
  * @phase process-sources
+ * @goal author
  */
 public class AuthorMojo extends AbstractMojo {
 
@@ -44,13 +44,7 @@ public class AuthorMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException {
 
         JavaDocBuilder builder = new JavaDocBuilder();
-        ClassLibrary lib = builder.getClassLibrary();
-        try {
-            // TODO scan for sources and add them
-            builder.addSource(sourceDirectory);
-        } catch (IOException e) {
-            throw new MojoExecutionException("Problem with the source directory.", e);
-        }
+        builder.addSourceTree(sourceDirectory);
 
         JavaClass[] classess = builder.getClasses();
         for (JavaClass cls : classess) {
@@ -62,11 +56,13 @@ public class AuthorMojo extends AbstractMojo {
                 if (getLog().isDebugEnabled()) {
                     getLog().warn("author found in source: " + author.getValue());
                 }
-                List<Developer> developers = project.getDevelopers();
-                for (Developer developer : developers) {
-                    if (containDeveloper(author.getValue(), developer)) {
-                        addClass(developer, cls);
-                    }
+
+                Set<Developer> developers = getDevelopers(author.getValue());
+                if (developers.isEmpty()) {
+                    getLog().warn(author.getValue() + " is an unknown developer. You must add him in pom.xml file, section <developers>.");
+
+                } else {
+                    addClass(developers, cls);
                 }
             }
         }
@@ -75,25 +71,35 @@ public class AuthorMojo extends AbstractMojo {
         getLog().warn(authorsFiles.toString());
     }
 
-    private static boolean containDeveloper(final String authorValue, final Developer developer) {
-        if (authorValue.contains(developer.getId())) {
-            return true;
+    private Set<Developer> getDevelopers(final String authorValue) {
+        Set<Developer> result = new TreeSet<Developer>();
+        List<Developer> developers = project.getDevelopers();
+        for (Developer developer : developers) {
+            if (authorValue.contains(developer.getId())) {
+                result.add(developer);
+                continue;
+            }
+            if (authorValue.contains(developer.getEmail())) {
+                result.add(developer);
+                continue;
+            }
+            if (authorValue.contains(developer.getName())) {
+                result.add(developer);
+                continue;
+            }
         }
-        if (authorValue.contains(developer.getEmail())) {
-            return true;
-        }
-        if (authorValue.contains(developer.getName())) {
-            return true;
-        }
-        return false;
+
+        return result;
     }
 
-    private void addClass(final Developer developer, final JavaClass cls) {
-        List<JavaClass> classes = authorsFiles.get(developer);
-        if (classes == null) {
-            classes = new ArrayList<JavaClass>();
-            authorsFiles.put(developer, classes);
+    private void addClass(final Set<Developer> developers, final JavaClass cls) {
+        for (Developer developer : developers) {
+            List<JavaClass> classes = authorsFiles.get(developer);
+            if (classes == null) {
+                classes = new ArrayList<JavaClass>();
+                authorsFiles.put(developer, classes);
+            }
+            classes.add(cls);
         }
-        classes.add(cls);
     }
 }
